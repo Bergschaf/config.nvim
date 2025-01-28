081d9a04f7716447608ea7953aa51aa2a9ec1c43
Bergschaf <christian.k@netcom-mail.de>
2025-01-28 21:27:48 +0100

    uuu

--- init.lua
+++ init.lua
@@ -970,5 +970,6 @@ require('lazy').setup({
   },
 })
 
+vim.api.nvim_create_autocmd({ 'FIleType' }, { pattern = 'tex', group = optional_group, command = 'TSBufDisable highlight' })
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
